36fd1d141a74f59faed79d7c8c18270e417f47a9
demkivroman <demkivroman5@gmail.com>
2017-11-14 21:13:22 +0200

    Add paginator & table filels grouping_by and reverse

--- students/views/students.py
+++ students/views/students.py
@@ -3,11 +3,36 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
 from ..models import Students
 
 # Views for Students
 def students_list(request):
     students = Students.objects.all()
+
+    # try to order students list
+    order_by = request.GET.get('order_by', 'last_name')
+    
+    if order_by in ('photo','last_name','first_name','ticket'):
+        students = students.order_by(order_by)
+        if request.GET.get('reverse','') == '1':
+            students = students.reverse() 
+    
+    # paginate students 
+    paginator = Paginator(students, 3)
+    page = request.GET.get('page')
+    try:
+        students = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        students = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver 
+        # last page of results.
+        students = paginator.page(paginator.num_pages)
+    
     return render(request,'students/students_list.html',{'students': students})
 
 
