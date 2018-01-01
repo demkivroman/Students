ad3a230877c3c87f6ec6a99e6765cb575b259d1f
demkivroman <demkivroman5@gmail.com>
2018-01-01 17:31:39 +0200

    configuration of admin part

--- students/admin.py
+++ students/admin.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+from django.core.urlresolvers import reverse
 from django.contrib import admin
 from .models.students import Students
 from .models.groups import Group
 from .models.model_exams import Exams
 # Register your models here.
 
-admin.site.register(Students)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['last_name','first_name','ticket','student_group']
+    list_display_links = ['last_name','first_name']
+    list_editable = ['student_group']
+    ordering = ['last_name']
+    list_filter = ['student_group']
+    list_per_page = 10
+    search_fields = ['last_name','first_name','middle_name','ticket','notes']
+
+    def view_on_site(self, obj):
+        return reverse('students_edit', kwargs = {'pk': obj.id})
+
+admin.site.register(Students,StudentAdmin)
 admin.site.register(Group)
 admin.site.register(Exams)
