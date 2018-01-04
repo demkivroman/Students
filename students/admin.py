bf7ca3977e5f58eb26be056ef08abe6d02f5ead0
demkivroman <demkivroman5@gmail.com>
2018-01-04 21:43:56 +0200

    Admin extension version

--- students/admin.py
+++ students/admin.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 from django.core.urlresolvers import reverse
 from django.contrib import admin
+from django.forms import ModelForm, ValidationError
 from .models.students import Students
 from .models.groups import Group
 from .models.model_exams import Exams
-# Register your models here.
 
+class StudentFormAdmin(ModelForm):
+    
+    def clean_student_group(self):
+        """ Check if student is leader in any group
+            If yes, then ensure it's the same as selected group. """
+
+        # get group where current student is leader
+        groups = Group.objects.filter(leader = self.instance)
+        if len(groups) > 0 and self.cleaned_data['student_group'] != groups[0]:
+            raise ValidationError(u'Студент є старостою іншої групи', code='invalid')
+        
+        return self.cleaned_data['student_group'] 
+        
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['last_name','first_name','ticket','student_group']
     list_display_links = ['last_name','first_name']
@@ -14,10 +27,21 @@ class StudentAdmin(admin.ModelAdmin):
     list_filter = ['student_group']
     list_per_page = 10
     search_fields = ['last_name','first_name','middle_name','ticket','notes']
+    form = StudentFormAdmin
 
     def view_on_site(self, obj):
         return reverse('students_edit', kwargs = {'pk': obj.id})
 
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['title','leader','note']
+    list_display_links = ['title']
+    list_editable = ['leader'] 
+    ordering = ['title']
+    list_filter = ['title']
+    list_per_page = 10
+    search_fields = ['title','leader']
+   
+
 admin.site.register(Students,StudentAdmin)
-admin.site.register(Group)
+admin.site.register(Group,GroupAdmin)
 admin.site.register(Exams)
