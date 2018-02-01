b2dd31bbfc3de14a4f98790f58c18ec757e1b15d
demkivroman <demkivroman5@gmail.com>
2018-02-01 21:05:12 +0200

    Add calendar vidget to field

--- students/static/js/main.js
+++ students/static/js/main.js
@@ -45,7 +45,17 @@ return true;
 });
 }
 
+
+function initDateFields(){
+  $("input.dateinput").datetimepicker({
+    'format': 'YYYY-MM-DD' 
+}).on('dp.hide',function(event){
+   $(this).blur(); 
+});
+}
+
 $(document).ready(function(){
 initJournal();
 initGroupSelector();
+initDateFields();
 });
